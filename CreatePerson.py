00d49c1af965777b21d6b0a703a64dd1c1d8bd7e
yelleen <yumizu@naver.com>
2020-11-22 15:58:17 +0900

    createPerson

---
+++ CreatePerson.py
@@ -0,0 +1,423 @@
+import random
+
+def n_s_male():
+    rn = 0
+    rn = random.randrange(1, 29)
+    fn = ''
+    sn = ''
+    tn = ''
+    fullName = ''
+    if rn == 1:
+        fn = '기'
+    elif rn == 2:
+        fn = '김'
+    elif rn == 3:
+        fn = '나'
+    elif rn == 4:
+        fn = '마'
+    elif rn == 5:
+        fn = '맹'
+    elif rn == 6:
+        fn = '박'
+    elif rn == 7:
+        fn = '방'
+    elif rn == 8:
+        fn = '상'
+    elif rn == 9:
+        fn = '석'
+    elif rn == 10:
+        fn = '선'
+    elif rn == 11:
+        fn = '설'
+    elif rn == 12:
+        fn = '손'
+    elif rn == 13:
+        fn = '송'
+    elif rn == 14:
+        fn = '안'
+    elif rn == 15:
+        fn = '양'
+    elif rn == 16:
+        fn = '염'
+    elif rn == 17:
+        fn = '오'
+    elif rn == 18:
+        fn = '원'
+    elif rn == 19:
+        fn = '유'
+    elif rn == 20:
+        fn = '윤'
+    elif rn == 21:
+        fn = '은'
+    elif rn == 22:
+        fn = '이'
+    elif rn == 23:
+        fn = '장'
+    elif rn == 24:
+        fn = '전'
+    elif rn == 25:
+        fn = '정'
+    elif rn == 26:
+        fn = '최'
+    elif rn == 27:
+        fn = '현'
+    elif rn == 28:
+        fn = '황'    
+    rn = random.randrange(1, 21)
+    if rn == 1:
+        sn = '석'
+    elif rn == 2:
+        sn = '주'
+    elif rn == 3:
+        sn = '경'
+    elif rn == 4:
+        sn = '한'
+    elif rn == 5:
+        sn = '규'
+    elif rn == 6:
+        sn = '당'
+    elif rn == 7:
+        sn = '진'
+    elif rn == 8:
+        sn = '태'
+    elif rn == 9:
+        sn = '현'
+    elif rn == 10:
+        sn = '삼'
+    elif rn == 11:
+        sn = '윤'
+    elif rn == 12:
+        sn = '시'
+    elif rn == 13:
+        sn = '도'
+    elif rn == 14:
+        sn = '훈'
+    elif rn == 15:
+        sn = '서'
+    elif rn == 16:
+        sn = '철'
+    elif rn == 17:
+        sn = '대'
+    elif rn == 18:
+        sn = '수'
+    elif rn == 19:
+        sn = '미'
+    elif rn == 20:
+        sn = '기'
+    rn = random.randrange(1, 21)
+    if rn == 1:
+        tn = '동'
+    elif rn == 2:
+        tn = '성'
+    elif rn == 3:
+        tn = '근'
+    elif rn == 4:
+        tn = '희'
+    elif rn == 5:
+        tn = '복'
+    elif rn == 6:
+        tn = '도'
+    elif rn == 7:
+        tn = '창'
+    elif rn == 8:
+        tn = '제'
+    elif rn == 9:
+        tn = '현'
+    elif rn == 10:
+        tn = '영'
+    elif rn == 11:
+        tn = '일'
+    elif rn == 12:
+        tn = '성'
+    elif rn == 13:
+        tn = '운'
+    elif rn == 14:
+        tn = '재'
+    elif rn == 15:
+        tn = '우'
+    elif rn == 16:
+        tn = '찰'
+    elif rn == 17:
+        tn = '윤'
+    elif rn == 18:
+        tn = '유'
+    elif rn == 19:
+        tn = '오'
+    elif rn == 20:
+        tn = '형'
+    fullName = fn + sn + tn
+    return fullName
+
+
+def n_s_female():
+    rn = 0
+    rn = random.randrange(1, 29)
+    fn = ''
+    sn = ''
+    tn = ''
+    fullName = ''
+    if rn == 1:
+        fn = '기'
+    elif rn == 2:
+        fn = '김'
+    elif rn == 3:
+        fn = '나'
+    elif rn == 4:
+        fn = '마'
+    elif rn == 5:
+        fn = '맹'
+    elif rn == 6:
+        fn = '박'
+    elif rn == 7:
+        fn = '방'
+    elif rn == 8:
+        fn = '상'
+    elif rn == 9:
+        fn = '석'
+    elif rn == 10:
+        fn = '선'
+    elif rn == 11:
+        fn = '설'
+    elif rn == 12:
+        fn = '손'
+    elif rn == 13:
+        fn = '송'
+    elif rn == 14:
+        fn = '안'
+    elif rn == 15:
+        fn = '양'
+    elif rn == 16:
+        fn = '염'
+    elif rn == 17:
+        fn = '오'
+    elif rn == 18:
+        fn = '원'
+    elif rn == 19:
+        fn = '유'
+    elif rn == 20:
+        fn = '윤'
+    elif rn == 21:
+        fn = '은'
+    elif rn == 22:
+        fn = '이'
+    elif rn == 23:
+        fn = '장'
+    elif rn == 24:
+        fn = '전'
+    elif rn == 25:
+        fn = '정'
+    elif rn == 26:
+        fn = '최'
+    elif rn == 27:
+        fn = '현'
+    elif rn == 28:
+        fn = '황'
+    rn = random.randrange(1, 21)
+    if rn == 1:
+        sn = '정'
+    elif rn == 2:
+        sn = '달'
+    elif rn == 3:
+        sn = '애'
+    elif rn == 4:
+        sn = '필'
+    elif rn == 5:
+        sn = '기'
+    elif rn == 6:
+        sn = '유'
+    elif rn == 7:
+        sn = '부'
+    elif rn == 8:
+        sn = '희'
+    elif rn == 9:
+        sn = '경'
+    elif rn == 10:
+        sn = '해'
+    elif rn == 11:
+        sn = '서'
+    elif rn == 12:
+        sn = '연'
+    elif rn == 13:
+        sn = '현'
+    elif rn == 14:
+        sn = '선'
+    elif rn == 15:
+        sn = '보'
+    elif rn == 16:
+        sn = '소'
+    elif rn == 17:
+        sn = '하'
+    elif rn == 18:
+        sn = '민'
+    elif rn == 19:
+        sn = '지'
+    elif rn == 20:
+        sn = '솔'
+    rn = random.randrange(1, 21)
+    if rn == 1:
+        tn = '선'
+    elif rn == 2:
+        tn = '래'
+    elif rn == 3:
+        tn = '리'
+    elif rn == 4:
+        tn = '영'
+    elif rn == 5:
+        tn = '연'
+    elif rn == 6:
+        tn = '원'
+    elif rn == 7:
+        tn = '태'
+    elif rn == 8:
+        tn = '빈'
+    elif rn == 9:
+        tn = '란'
+    elif rn == 10:
+        tn = '정'
+    elif rn == 11:
+        tn = '이'
+    elif rn == 12:
+        tn = '린'
+    elif rn == 13:
+        tn = '희'
+    elif rn == 14:
+        tn = '윤'
+    elif rn == 15:
+        tn = '옥'
+    elif rn == 16:
+        tn = '림'
+    elif rn == 17:
+        tn = '라'
+    elif rn == 18:
+        tn = '겸'
+    elif rn == 19:
+        tn = '화'
+    elif rn == 20:
+        tn = '아'
+    fullName = fn + sn + tn
+    return fullName
+
+def cityrand():
+    strcity = ''
+    rc = random.randrange(1, 37)
+    if rc == 1:
+        strcity = '서울시 종로구'
+    elif rc == 2:
+        strcity = '서울시 중구'
+    elif rc == 3:
+        strcity = '서울시 강남구'
+    elif rc == 4:
+        strcity = '서울시 은평구'
+    elif rc == 5:
+        strcity = '서울시 마포구'
+    elif rc == 6:
+        strcity = '서울시 양천구'
+    elif rc == 7:
+        strcity = '서울시 구로구'
+    elif rc == 8:
+        strcity = '서울시 서대문구'
+    elif rc == 9:
+        strcity = '서울시 관악구'
+    elif rc == 10:
+        strcity = '서울시 송파구'
+    elif rc == 11:
+        strcity = '인천시 중구'
+    elif rc == 12:
+        strcity = '인천시 동구'
+    elif rc == 13:
+        strcity = '인천시 미추홀구'
+    elif rc == 14:
+        strcity = '인천시 연수구'
+    elif rc == 15:
+        strcity = '인천시 남동구'
+    elif rc == 16:
+        strcity = '인천시 부평구'
+    elif rc == 17:
+        strcity = '인천시 계양구'
+    elif rc == 18:
+        strcity = '인천시 서구'
+    elif rc == 19:
+        strcity = '경기도 수원시'
+    elif rc == 20:
+        strcity = '경기도 성남시'
+    elif rc == 21:
+        strcity = '경기도 안양시'
+    elif rc == 22:
+        strcity = '경기도 안산시'
+    elif rc == 23:
+        strcity = '경기도 용인시'
+    elif rc == 24:
+        strcity = '경기도 부천시'
+    elif rc == 25:
+        strcity = '경기도 광명시'
+    elif rc == 26:
+        strcity = '경기도 평택시'
+    elif rc == 27:
+        strcity = '경기도 시흥시'
+    elif rc == 28:
+        strcity = '경기도 군포시'
+    elif rc == 29:
+        strcity = '경기도 의왕시'
+    elif rc == 30:
+        strcity = '경기도 하남시'
+    elif rc == 31:
+        strcity = '경기도 이천시'
+    elif rc == 32:
+        strcity = '경기도 안성시'
+    elif rc == 33:
+        strcity = '경기도 김포시'
+    elif rc == 34:
+        strcity = '경기도 화성시'
+    elif rc == 35:
+        strcity = '경기도 광주시'
+    elif rc == 36:
+        strcity = '경기도 여주시'
+    return strcity
+
+def randTimeSet():
+    randInHour = random.randrange(10, 20)
+    randInMin = random.randrange(0, 60)
+    randStay = random.randrange(10, 30)
+    randOutHour = randInHour
+    randOutMin = randInMin + randStay
+    if randOutMin >= 60:
+        randOutMin = randOutMin - 60
+        randOutHour = randOutHour + 1
+    result = str(randInHour) + ':' + str(randInMin) + ' ' + str(randOutHour) + ':' + str(randOutMin)
+    return result
+
+def randRoomSet():
+    randRoom = random.randrange(1, 5)
+    return str(randRoom)
+
+def fileCreation(num):
+    dataPath = "Data/2020_11_" + str(num) + ".txt"
+    fe = open(dataPath, 'w')
+
+    for s in range(1, 101):
+        strname = n_s_female()
+        ge = '여성'
+        randage = random.randrange(16, 81)
+        rand1 = random.randrange(1000, 9999)
+        rand2 = random.randrange(1000, 9999)
+        city = cityrand()
+        time = randTimeSet()
+        room = randRoomSet()
+        str_person = strname + ' ' + str(randage) + ' ' + ge + ' 010-' + str(rand1) + '-' + str(rand2) + ' ' + city + ' ' + time + ' ' +  room + '\n'
+        fe.write(str_person)
+    for s in range(1, 101):
+        strname = n_s_male()
+        ge = '남성'
+        randage = random.randrange(16, 81)
+        rand1 = random.randrange(1000, 9999)
+        rand2 = random.randrange(1000, 9999)
+        city = cityrand()
+        time = randTimeSet()
+        room = randRoomSet()
+        str_person = strname + ' ' + str(randage) + ' ' + ge + ' 010-' + str(rand1) + '-' + str(rand2) + ' ' + city + ' ' + time + ' ' +  room + '\n'
+        fe.write(str_person)
+
+    fe.close()
+
+for i in range(1, 16):
+    fileCreation(i)
