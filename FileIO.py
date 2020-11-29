440d80c471a55afe85fff9159a172f715101cc3b
yelleen <yumizu@naver.com>
2020-11-29 21:40:29 +0900

    GUI

--- FileIO.py
+++ FileIO.py
@@ -1,4 +1,5 @@
 import os
+from GUI_yelleen import fileName
 class Person:
     def __init__(self, name, age, gender, phone_number, address1, address2, time_in, time_out, roomNumber):
         self.name = name
@@ -14,27 +15,23 @@ class Person:
         personStr = self.name + ' ' + str(self.age) + ' ' + self.gender + ' ' + self.phone_number + ' ' + self.address1 + ' ' + self.address2 + ' ' + self.time_in + ' ' + self.time_out + ' ' + self.roomNumber + '\n'
         return personStr
 
-def AddPersonToFile(fileName, person): # in order to write file and add person
-    dataPath = "Data/" + fileName + ".txt"
-    dataFile = open(dataPath, 'a')
-    dataStr = person.toString()
-    dataFile.write(dataStr)
+def AddPersonToFile(fileName, pList): # in order to write file and add person
+    dataFile = open(fileName, 'a')
+    for p in pList:
+        pStr = p.toString()
+        dataFile.write(pStr)
     dataFile.close()
 
+def ReadText(fileName):
+    peopleStr = open(fileName, 'r')
+    lines = peopleStr.readlines
+    peopleStr.close()
+    return lines
 
-def ReadTxt():
-    people_list = []
-    people_txt = open("people.txt","r")
-    lines = people_txt.readlines
-    i = 0
-    for line in lines:
-        per_list = line.split(' ')
-        list_name = "people" + i
-        list_name = Person(per_list[0], per_list[1], per_list[2], per_list[3], per_list[4], per_list[5], per_list[6], per_list[7], per_list[8])
-        people_list.append(list_name)
-        i += 1
-    people_txt.close()
-    return people_list
+def StringToPerson(pstr, pList):
+    pe = pstr.split(' ')
+    p = Person(pe[0], pe[1], pe[2], pe[3], pe[4], pe[5], pe[6], pe[7], pe[8])
+    pList.append(p)
 
 def TimeSet(person):
     time = []
@@ -52,7 +49,7 @@ def TimeSet(person):
     return time # 시간, 분, 시간, 분
 
 def name_check(name,people_list):#이름 중복 체크
-    people_list = ReadTxt()
+    people_list = ReadText(fileName)
     check = []
     index = 0
     for i in people_list:
@@ -65,10 +62,10 @@ def name_check(name,people_list):#이름 중복 체크
         return check.pop(0)
 
 def phone_num(index):#해당자의 전화번호(문자열) 출력
-    people_list = ReadTxt()
+    people_list = ReadText(fileName)
     person_class = people_list[index]
     return person_class.phone_number
 
 def PersonClass(index):#해당자의 class 반환
-    people_list = ReadTxt()
+    people_list = ReadText(fileName)
     return people_list[index]
--- GUI_yelleen.py
+++ GUI_yelleen.py
@@ -1,34 +1,94 @@
-#from FileIO import *
-#from Graph import *
-#from Filtering import *
 from GUI_Info import *
-from tkinter import Tk, Label, Button, Entry, ttk
-
-def entryToString():
-    s = str(entryName.get()) + ' ' + str(entryAge.get()) + ' ' + str(comboboxGender.get()) + ' ' + str(entryPhoneNumber.get()) + ' ' + str(comboboxAdr1.get()) + ' ' + str(comboboxAdr2.get())
-    return s
+from FileIO import *
+from tkinter import *
+from tkinter import ttk, messagebox
+from tkinter import filedialog
 
 window = Tk()
 window.title("CO19")
 window.geometry("1280x720")
 window.resizable(False, False)
 
-labelName = Label(window, text = "이름", width = 4, height = 1, font = "나눔바른고딕 -28")
+fileName = ""
+People = []
+pe = []
+phoneCorrect = True
+#Region Registering
+def isCorrectPhoneNumber():
+    global phoneCorrect
+    phoneStr = str(entryPhoneNumber.get())
+    if len(phoneStr) != 13:
+        phoneCorrect = False
+    else:
+        for i in range(13):  
+            if phoneStr[i] == '-' and (i == 3 or i == 8):
+                phoneCorrect = False
+            elif phoneStr[i].isdigit():
+                phoneCorrect = False
+    WarningPhone()
+def WarningPhone():
+    global phoneCorrect
+    if phoneCorrect == False:
+        result = messagebox.showinfo(title = "경고", message = "전화번호가 잘못 입력되었습니다.\n다시 입력해주세요.\n'010-####-####'")
+    else:
+        Register()
+
+def entryToList():
+    global pe
+    pe.append(str(entryName.get()))
+    pe.append(str(entryAge.get()))
+    pe.append(str(comboboxGender.get()))
+    pe.append(str(entryPhoneNumber.get()))
+    pe.append(str(comboboxAdr1.get()))
+    pe.append(str(comboboxAdr2.get()))
+
+def Register():
+    global People, pe
+    p = Person(pe[0], pe[1], pe[2], pe[3], pe[4], pe[5], pe[6], pe[7], pe[8])
+    People.append(p)
+#EndRegion
+#Region Finding
+def Find(pe):
+    global People
+#EndRegion
+#Region Save
+def Save():
+    global People, fileName
+    fileName = filedialog.askopenfilename(initaldir = '/', title = "Select file", filetypes = (("text files", "*.txt"), ("all files", "*.*")))
+    AddPersonToFile(fileName, People)
+#EndRegion
+#Region Load
+def Load(pe):
+    global People
+    fileName = filedialog.askopenfilename(initaldir = '/', title = "Select file", filetypes = (("text files", "*.txt"), ("all files", "*.*")))
+    pStrList = ReadText(fileName)
+    for pStr in pStrList:
+        StringToPerson(pStr, People)
+#EndRegion
+
+'''
+=====================================================================================
+'''
+
+btn_register = Button(window, text = "등록하기", command = Register)
+btn_finding = Button(window, text = "찾기", command = Find)
+
+labelName = Label(window, text = "이름", width = 4, height = 1, font = "나눔바른고딕 -28", anchor="nw")
 entryName = Entry(window, width = 15, font = "나눔바른고딕 -28")
 
-labelAge = Label(window, text = "나이", width = 4, height = 1, font = "나눔바른고딕 -28")
+labelAge = Label(window, text = "나이", width = 4, height = 1, font = "나눔바른고딕 -28", anchor="nw")
 entryAge = Entry(window, width = 15, font = "나눔바른고딕 -28")
 
 genderValue = gender
-labelGender = Label(window, text = "성별", width = 4, height = 1, font = "나눔바른고딕 -28")
-comboboxGender = ttk.Combobox(window, height = 15, values = genderValue)
+labelGender = Label(window, text = "성별", width = 4, height = 1, font = "나눔바른고딕 -28", anchor="nw")
+comboboxGender = ttk.Combobox(window, height = 15, values = genderValue, font = "나눔바른고딕 -28")
 
-labelPhoneNumber = Label(window, text = "전화번호", width = 8, height = 1, font = "나눔바른고딕 -28")
+labelPhoneNumber = Label(window, text = "전화번호", width = 8, height = 1, font = "나눔바른고딕 -28", anchor="nw")
 entryPhoneNumber = Entry(window, width = 15, font = "나눔바른고딕 -28")
 
 adr1Value = adr1
-labelAddress1 = Label(window, text = "주소", width = 4, height = 1, font = "나눔바른고딕 -28")
-comboboxAdr1 = ttk.Combobox(window, height = 15, values = adr1Value)
+labelAddress1 = Label(window, text = "주소", width = 4, height = 1, font = "나눔바른고딕 -28", anchor="nw")
+comboboxAdr1 = ttk.Combobox(window, height = 15, values = adr1Value, font = "나눔바른고딕 -26")
 
 adr2Value = ()
 if str(comboboxAdr1.get()) == adr1[0]:
@@ -37,8 +97,9 @@ elif str(comboboxAdr1.get()) == adr1[1]:
     adr2Value = adrB
 else:
     adr2Value = adrC
+
 labelAddress2 = Label(window, text = " ", width = 1, height = 1, font = "나눔바른고딕 -28")
-comboboxAdr2 = ttk.Combobox(window, height = 15, values = adr2Value)
+comboboxAdr2 = ttk.Combobox(window, height = 15, values = adr2Value, font = "나눔바른고딕 -26")
 
 labelTimeIn = Label(window, text = "들어온 시간", width = 8, height = 1, font = "나눔바른고딕 -28")
 entryTimeIn = Entry(window, width = 8, font = "나눔바른고딕 -28")
@@ -46,37 +107,25 @@ entryTimeIn = Entry(window, width = 8, font = "나눔바른고딕 -28")
 labelTimeOut = Label(window, text = "나간 시간", width = 8, height = 1, font = "나눔바른고딕 -28")
 entryTimeOut = Entry(window, width = 8, font = "나눔바른고딕 -28")
 
-def test():
-    print(str(comboboxGender.get()))
-
-btn = Button(window, width = 20, command = test)
-
-btn.pack()
-labelName.pack()
-entryName.pack()
-labelAge.pack()
-entryAge.pack()
-labelGender.pack()
-comboboxGender.pack()
-labelPhoneNumber.pack()
-entryPhoneNumber.pack()
-labelAddress1.pack()
-comboboxAdr1.pack()
-labelAddress2.pack()
-comboboxAdr2.pack()
-
-
-
-
 '''
-labelName.place(x = 20, y = 50)
-entryName.place(x = 20, y = 90)
-labelPhoneNumber.place(x = 270, y = 50)
-entryPhoneNumber.place(x = 270, y = 90)
-labelAddress1.place(x = 520, y = 50)
-entryAddress1.place(x = 520, y = 90)
-labelAddress2.place(x = 658, y = 50)
-entryAddress2.place(x = 658, y = 90)
+========================================================================
 '''
+labelName.place(x = 100, y = 100, width=100, height=30)# 명칭 표시
+entryName.place(x = 100, y = 150, width=200, height=50)# 입력란 표시
+labelAge.place(x = 400, y = 100, width=100, height=30)
+entryAge.place(x = 400, y = 150, width=100, height=50)
+labelGender.place(x = 600, y = 100, width=100, height=30)
+comboboxGender.place(x = 600, y = 150, width=100, height=50)
+labelAddress1.place(x = 800, y = 100, width=100, height=30)
+comboboxAdr1.place(x = 800, y = 150, width=150, height=50)
+comboboxAdr2.place(x = 960, y = 150, width=150, height=50)
+
+#두번쨰 줄 표시할 것들 
+labelPhoneNumber.place(x = 100, y = 250, width=120, height=50)
+entryPhoneNumber.place(x = 100, y = 300, width=250, height=50)
+
+btn = Button(window, width = 20, command = isCorrectPhoneNumber)
+btn.place(x = 400, y = 250, width=150, height=50)
+
 
 window.mainloop()
