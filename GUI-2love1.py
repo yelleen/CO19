4a52ef2a9ce81cf716439a0d676a2fbd2b85831f
newoldname <thjbjs@outlook.com>
2020-11-29 16:39:16 +0900

    gui

---
+++ GUI-2love1.py
@@ -0,0 +1,12 @@
+from FileIO import *
+from Graph import *
+from Filtering import *
+from tkinter import *
+
+window = Tk()
+window.title("CO19")
+window.geometry("1280x720")
+
+
+
+window.mainloop()
